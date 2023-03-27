58e89d36abddfe0c9c237220c0157948102cc1a8
Jade Stewart <jadekstewart3@gmail.com>
2023-03-27 15:57:00 -0600

    Complete testing for finding all items, finding a single item, and creating a new item

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,79 @@
+require "rails_helper"
+
+describe "Items API" do 
+  it "can get all items" do 
+    create_list(:item, 10)
+
+    get "/api/v1/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items.count).to eq(10)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(Float)
+
+      expect(item).to have_key(:merchant_id)
+      expect(item[:merchant_id]).to be_an(Integer)
+    end
+  end
+
+  it 'can get one item by id' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+   
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(Integer)
+
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_an(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_an(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_an(Float)
+
+    expect(item).to have_key(:merchant_id)
+    expect(item[:merchant_id]).to be_an(Integer)
+  end
+
+  it "can create a new item" do
+    merchant = create(:merchant)
+    item_params =({id: 82,
+                  name: "Jospeh",
+                  description: "The programs that Chuck Norris writes don't have version numbers because he only writes them once. If a user reports a bug or has a feature request they don't live to see the sun set.",
+                  unit_price: 42.91,
+                  merchant_id: merchant.id
+                })
+    headers = {"CONTENT_TYPE" => "application/json"}
+    
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+    created_item = Item.last
+    
+    expect(response).to be_successful
+    expect(created_item.id).to eq(item_params[:id])
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+end
