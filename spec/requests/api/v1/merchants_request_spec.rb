f361689388f1152043decd534e858fdbc166e041
Jade Stewart <jadekstewart3@gmail.com>
2023-03-27 15:55:07 -0600

    Complete testing for finding all merchants and finding a single merchant by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,7 +9,32 @@ describe "Merchants API" do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
-
+    
     expect(merchants.count).to eq(5)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_an(String)
+    end
+  end
+
+  it "can get one merchant by id" do 
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(Integer)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_an(String)
+  
   end
 end
