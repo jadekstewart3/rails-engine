94d37688820f9538bb330b782fc30e0adf15ce1a
Jade Stewart <jadekstewart3@gmail.com>
2023-03-27 16:09:26 -0600

    Add index, show, create, and item_params to Items Controller

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    render json: Item.all
+  end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+
+    render json: Item.create!(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:id, :name, :description, :unit_price, :merchant_id )
+  end
+end
