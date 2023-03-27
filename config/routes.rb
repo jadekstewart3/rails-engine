8f07af9c230970e134bae1d03c8c6881df62ab6b
Jade Stewart <jadekstewart3@gmail.com>
2023-03-27 17:41:23 -0600

    Add routes and serializer gem

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do 
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: :index
+      end
     end
   end
 
