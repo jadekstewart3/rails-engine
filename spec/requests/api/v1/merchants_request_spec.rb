6bc9dbade91b6150c2af203ece37957930294a2c
Jade Stewart <jadekstewart3@gmail.com>
2023-03-28 21:03:04 -0600

    Add tests for merchants keyword search

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -40,11 +40,49 @@ describe "Merchants API" do
     end
 
     it "will return an error if the merchant does not exist" do 
-      get "/api/v1/merchants/1"
+      get "/api/v1/merchants/1272327185"
+      
+      merchant = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to_not be_successful
-      expect(response).to have_status(404)
+      expect(response).to have_http_status(404)
+      expect(merchant).to have_key(:message)
+      expect(merchant).to have_key(:errors)
+      expect(merchant[:message]).to eq("your query could not be completed")
+      expect(merchant[:errors]).to eq("The Merchant ID does not exist")
+    end
+  end
 
+  describe "#find" do
+    before :each do
+      @ring_world = Merchant.create!(name: "Ring World")
+      @ring_worm = Merchant.create!(name: "Ring Worm")
+      @bling_ring = Merchant.create!(name: "Bling Ring")
+    end
+    
+    it "can return one merchant by keyword search" do
+      get "/api/v1/merchants/find?name=Ring"
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(merchants[:data]).to have_key(:id)
+      expect(merchants[:data][:id]).to be_a(String)
+
+      expect(merchants[:data][:attributes]).to have_key(:name)
+      expect(merchants[:data][:attributes][:name]).to be_a(String)
+    end
+
+    it "will return an error if no merchant matches the search" do
+      get "/api/v1/merchants/find?name=cat"
+      merchant = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(404)
+      expect(merchant).to have_key(:message)
+      expect(merchant).to have_key(:errors)
+      expect(merchant[:message]).to eq("your query could not be completed")
+      expect(merchant[:errors]).to eq("No Merchant matches your search")
     end
   end
 end
