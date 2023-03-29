6738ad4ba54f6e54b9e8aa21ad502e39b54e4156
Jade Stewart <jadekstewart3@gmail.com>
2023-03-29 12:52:04 -0600

    Adjust model methods to allow for range of price

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,9 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
 
-  validates :unit_price, numericality: true
+  validates_numericality_of :unit_price, greater_than: 0
   validates :name, :description, presence: true
-  
+
   def get_one_item_invoices
     invoices
     .joins(:invoice_items)
@@ -14,14 +14,24 @@ class Item < ApplicationRecord
   end
 
   def self.find_all_items_by_name(keyword)
-    where("name ILIKE ?", "%#{keyword}%")
+    where("name ILIKE ?", "%#{keyword}%").order(name: :asc)
   end
 
-  def self.find_items_by_min_price(min_value)
-    where("unit_price >= ?", "#{min_value}").order(unit_price: :desc)
-  end
+  # def self.find_items_by_min_price(min_value)
+  #   where("unit_price >= ?", "#{min_value}").order(unit_price: :desc)
+  # end
 
-  def self.find_items_by_max_price(max_value)
-    where("unit_price <= ?", "#{max_value}")
+  # def self.find_items_by_max_price(max_value)
+  #   where("unit_price <= ?", "#{max_value}").order(unit_price: :desc)
+  # end
+
+  def self.find_items_by_price(min, max)
+    if min != nil && max != nil
+      where("unit_price >= ? AND unit_price <= ?", min, max)
+    elsif min == nil
+      where("unit_price <= ?", max)
+    else  
+      where("unit_price >= ?", min)
+    end.order(name: :asc)
   end
 end
