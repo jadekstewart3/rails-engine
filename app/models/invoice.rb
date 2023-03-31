863eb721616f36d502a5fc6d42e09cff3b0cc997
Jade Stewart <jadekstewart3@gmail.com>
2023-03-31 09:52:08 -0600

    Add dependant destroy to transactions

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,5 +3,5 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
 end
